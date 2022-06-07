ba95340bc970896e00b668cdbae22d3a874ecba8
Dima-Sandler <dima1.san@gmail.com>
2022-06-08 02:05:29 +0300

    Add function checkEvenParity and update main

--- Assignment6/Source.c
+++ Assignment6/Source.c
@@ -10,12 +10,18 @@ int pack4Chars2Int(char c1, char c2, char c3, char c4) {
 	return num;
 }
 
+int checkEvenParity(short input) {
+	char count = 0;
+
+	for (int i = 0; i < 8 * sizeof(short); i++)
+		if (input >> i & 1)
+			count++;	
+
+	return !(count % 2);
+}
+
 void main() {
 	int res = pack4Chars2Int('a', 'b', 'c', 'd');
 	
-
-
-
-
-
+	printf("%d\n", checkEvenParity(0b1100011100001100));
 }
